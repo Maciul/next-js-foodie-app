87abc0bda0b5834c1a6160601c601d39e30b423c
Pawel Maciulewski <pawel.maciulewski@gmail.com>
2024-09-18 12:08:50 +0200

    change it to the foodie app

--- app/page.js
+++ app/page.js
@@ -1,43 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/link";
-import Header from "./components/header";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <Header />
-
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-        <Link href="/about" rel="noopener noreferrer">
-          About Us
-        </Link>
-      </main>
-    </div>
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+    </main>
   );
 }
