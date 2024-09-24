3606632f506e531d3178ef6e5505c66b276bde01
Pawel Maciulewski <pawel.maciulewski@gmail.com>
2024-09-24 21:09:05 +0200

    change the page view

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,22 @@
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1> Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}></header>
+      <h1>
+        Delicious meals, create
+        <span className={classes.highlight}>by you</span>
+      </h1>
+
+      <p>
+        Choose your favorite recipe and cook it yourself. It is easy and fun!{" "}
+      </p>
+
+      <p className={classes.cta}>
+        <Link href="/meals/share"> Share your favorite recipe</Link>
+      </p>
+      <main className={classes.main}></main>
+    </>
   );
 }
