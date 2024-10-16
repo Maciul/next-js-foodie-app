a06e388d1a55be3e66d4739e41742361700bc766
Pawel Maciulewski <pawel.maciulewski@gmail.com>
2024-10-16 12:43:33 +0200

    add not found, error and loading state

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1> Not Found</h1>
+      <p> This page was not found, try something else</p>
+    </main>
+  );
+}
