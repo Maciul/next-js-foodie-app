12e9beb8649f97cffcfa2a9fae10f56dbc26381f
Pawel Maciulewski <pawel.maciulewski@gmail.com>
2024-10-31 11:04:34 +0100

    add useRef for image picker button

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,16 +1,28 @@
+"use client";
+
 import classes from "./image-picker.module.css";
+import { useRef } from "react";
 
 export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+  function handleClick() {
+    imageInput.current.click();
+  }
   return (
     <div className={classes.picker}>
       <label htmlFor="image">{label}</label>
       <div className={classes.controls}>
         <input
+          className={classes.input}
           type="file"
           id={name}
           accept="image/png, image/jpeg"
           name={name}
+          ref={imageInput}
         />
+        <button className={classes.button} type="button" onClick={handleClick}>
+          Pick an Image
+        </button>
       </div>
     </div>
   );
